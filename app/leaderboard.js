b615c26e2b15477f103b86a112e02ce351a3a038
amaskey <maskey.maskey@gmail.com>
2017-12-20 15:26:35 -1000

    update latest

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,1 +1,55 @@
-console.log("Hello world");
+
+import { Mongo } from 'meteor/mongo';
+import { Template } from 'meteor/templating';
+
+
+export const PlayersList = new Mongo.Collection('players');
+
+//PlayersList not coz export const
+//To use PlayersList need to import { PlayersList } from './leaderboard.js'
+
+if (Meteor.isClient) {
+  Template.leaderboard.helpers({
+    'player': function () {
+      return PlayersList.find();
+    },
+    'count': function() {
+      return PlayersList.find().count();
+    },
+
+  });
+
+  Template.leaderboard.events({
+    'click .player': function() {
+      let playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+      console.log(Session.get('selectedPlayer'));
+    },
+
+    'dblclick .player': function() {
+      console.log("double clicked playa");
+    },
+
+    'focus': function() {
+      console.log("focus");
+    },
+
+    'blur': function() {
+      console.log("blur");
+    },
+
+    'mouseover': function() {
+      console.log("mouseover");
+    },
+
+    'change': function() {
+      console.log("change");
+
+    },
+
+  });
+}
+
+if (Meteor.isServer) {
+}
+
